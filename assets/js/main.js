6ab647366b45e039e79a51f892cb42678a24b13a
Mohammad Rahman <mbrahman@gmail.com>
2022-02-09 19:46:33 -0500

    finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,6 +12,9 @@ var cityInputEl = document.querySelector('#city-input')
 // history section
 var historyDivEl = document.querySelector('#history')
 
+// todays weather section
+var todaysWeatherDivEl = document.querySelector('#todays-weather')
+
 var apiKey = 'a08a76e0b432a1339ec48baaa41272b8'
 
 var cityList = []
@@ -61,9 +64,9 @@ var getCityInfo = function (city) {
                 console.log(data)
                 // use lat lon to get fetch weather forecast, sinc the first API call does not
                 // include UVI or forecast data
-                getWeather(cityLat, cityLon)
+                getWeather(cityLat, cityLon, cityName)
                 saveCities()
-                renderHistory(cityList)
+                renderHistoryBtn(cityList)
             })
         } else {
             alert('Unable to find City')
@@ -73,12 +76,12 @@ var getCityInfo = function (city) {
     })
 }
 
-var getWeather = function (lat, lon) {
-    var apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`
+var getWeather = function (lat, lon, name) {
+    var apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`
     fetch(apiUrl).then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
-                console.log(data)
+                renderTodaysWeather(data, name)
             })
         } else {
             alert('Unable to get weather data')
@@ -86,7 +89,52 @@ var getWeather = function (lat, lon) {
     })
 }
 
-var renderHistory = function (cityList) {
+var renderTodaysWeather = function(weatherData, city) {
+    console.log(weatherData)
+
+    todaysWeatherDivEl.textContent = ''
+
+    var cityEl = document.createElement('h3')
+    cityEl.textContent = city
+    todaysWeatherDivEl.append(cityEl)
+
+    var temp = weatherData.daily[0].temp.day
+    var tempEl = document.createElement('p')
+    tempEl.textContent = `Temp: ${temp}`
+    todaysWeatherDivEl.append(tempEl)
+
+    var wind = weatherData.daily[0].wind_speed
+    var windEl = document.createElement('p')
+    windEl.textContent = `Wind: ${wind}`
+    todaysWeatherDivEl.append(windEl)
+
+    var humidity = weatherData.daily[0].humidity
+    var humidityEl = document.createElement('p')
+    humidityEl.textContent = `Humidity: ${humidity}`
+    todaysWeatherDivEl.append(humidityEl)
+
+    var uvi = weatherData.daily[0].uvi
+    var uviSpanEl = document.createElement('span')
+    uviSpanEl.textContent = uvi
+    uviSpanEl.setAttribute('style', 'padding: 5px')
+
+    if (uvi < 2) {
+        uviSpanEl.classList.add('bg-success')
+    } else if (uvi <= 7) {
+        uviSpanEl.classList.add('bg-warning')
+    } else {
+        uviSpanEl.classList.add('bg-danger')
+    }
+    
+
+    var uviEl = document.createElement('p')
+    uviEl.textContent = `UV Index: `
+    uviEl.append(uviSpanEl)
+    
+    todaysWeatherDivEl.append(uviEl)
+}
+
+var renderHistoryBtn = function (cityList) {
 
     historyDivEl.textContent = ''
 
@@ -95,7 +143,8 @@ var renderHistory = function (cityList) {
             var cityBtn = document.createElement('button')
             cityBtn.classList.add('btn', 'btn-outline-primary', 'btn-block', 'mb-2')
             cityBtn.textContent = cityList[i].city
-
+            cityBtn.setAttribute('data-city-id', cityList[i].id)
+            cityBtn.setAttribute('data-city-name', cityList[i].city)
             historyDivEl.append(cityBtn)
         }
     } else {
@@ -103,13 +152,20 @@ var renderHistory = function (cityList) {
             var cityBtn = document.createElement('button')
             cityBtn.classList.add('btn', 'btn-outline-primary', 'btn-block', 'mb-2')
             cityBtn.textContent = element.city
-
+            cityBtn.setAttribute('data-city-id', element.id)
+            cityBtn.setAttribute('data-city-name', element.city)
             historyDivEl.append(cityBtn)
         })
     }
 }
 
+var getHistory = function(event) {
+    var city = event.target.dataset.cityName
+    getCityInfo(city)
+}
+
 loadCities()
-renderHistory(cityList)
+renderHistoryBtn(cityList)
 
 cityFormEl.addEventListener('submit', formSubmitHandler)
+historyDivEl.addEventListener('click', getHistory)
