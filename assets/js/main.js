62f67e12f41489e9a99a92c23a1d6ee73c3bcec3
Mohammad Rahman <mbrahman@gmail.com>
2022-02-10 22:51:28 -0500

    finished rendering forecast

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,6 +15,9 @@ var historyDivEl = document.querySelector('#history')
 // todays weather section
 var todaysWeatherDivEl = document.querySelector('#todays-weather')
 
+// forecast section
+var forecastDivEl = document.querySelector('#forecast')
+
 var apiKey = 'a08a76e0b432a1339ec48baaa41272b8'
 
 var cityList = []
@@ -82,6 +85,7 @@ var getWeather = function (lat, lon, name) {
         if (response.ok) {
             response.json().then(function (data) {
                 renderTodaysWeather(data, name)
+                renderForecast(data, name)
             })
         } else {
             alert('Unable to get weather data')
@@ -98,7 +102,7 @@ var renderTodaysWeather = function (weatherData, city) {
     var unixTime = weatherData.daily[0].dt * 1000
     var time = new Date(unixTime).toLocaleDateString('en-US')
     var dateEl = document.createElement('span')
-    dateEl.textContent = ` (${time}) `
+    dateEl.textContent = ` (${time})`
 
     // add icon
     var icon = weatherData.daily[0].weather[0].icon
@@ -144,6 +148,47 @@ var renderTodaysWeather = function (weatherData, city) {
     uviEl.append(uviSpanEl)
 
     todaysWeatherDivEl.append(uviEl)
+}
+
+var renderForecast = function (weatherData, city) {
+
+    console.log(weatherData)
+
+    forecastDivEl.textContent = ''
+
+    for (let i = 1; i < 6; i++) {
+
+        var dayCardEl = document.createElement('div')
+        dayCardEl.classList.add('card', 'p-2', 'text-light')
+        dayCardEl.setAttribute('style', 'min-width: 11rem')
+        dayCardEl.style.backgroundColor = '#023047'
+
+        // add today's date
+        var unixTime = weatherData.daily[i].dt * 1000
+        var time = new Date(unixTime).toLocaleDateString('en-US')
+        var dateEl = document.createElement('span')
+        dateEl.textContent = `${time}`
+
+        // add icon
+        var icon = weatherData.daily[i].weather[0].icon
+        var iconEl = document.createElement('img')
+        iconEl.src = `http://openweathermap.org/img/wn/${icon}.png`
+
+        var temp = weatherData.daily[0].temp.day
+        var tempEl = document.createElement('p')
+        tempEl.textContent = `Temp: ${temp}`
+
+        var wind = weatherData.daily[0].wind_speed
+        var windEl = document.createElement('p')
+        windEl.textContent = `Wind: ${wind}`
+
+        var humidity = weatherData.daily[0].humidity
+        var humidityEl = document.createElement('p')
+        humidityEl.textContent = `Humidity: ${humidity}`
+
+        dayCardEl.append(dateEl, iconEl, tempEl, windEl, humidityEl)
+        forecastDivEl.append(dayCardEl)
+    }
 }
 
 var renderHistoryBtn = function (cityList) {
